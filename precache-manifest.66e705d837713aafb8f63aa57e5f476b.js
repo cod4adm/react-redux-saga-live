25386e4b80758f3f5fdce30196f49ba240d470ad
cod4adm <57834361+cod4adm@users.noreply.github.com>
2020-07-31 16:38:19 +0300

    live

---
+++ precache-manifest.66e705d837713aafb8f63aa57e5f476b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ffc100aaa62addbf22eb0c6e8eb6dc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e305f3a100f2a85b6f0b",
+    "url": "/static/js/2.22f8a5b7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.22f8a5b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f973a579780086cd7c30",
+    "url": "/static/js/main.8370227b.chunk.js"
+  },
+  {
+    "revision": "2fd127e97f3ae1e947fa",
+    "url": "/static/js/runtime-main.7fb42074.js"
+  }
+]);
